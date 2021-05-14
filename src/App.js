37d4f4f3127034b78d4906e160df3337c33e1537
Suhas Bhairav <suhasbhairav@gmail.com>
2021-05-14 16:06:36 +0200

    use reducer tutorials
    use reducer tutorials

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import React, {useState, useReducer} from "react";
 import './App.css';
 
 function App() {
+
+  const initialState = [{
+    id: Date.now(),
+    name: "Suhas Bhairav",
+    email: "test@test.com"
+  }];
+
+  function reducer(state, action){
+    console.log(state, action);
+    switch(action.type){
+      case "add":
+        return [action.payload, ...initialState, ];
+
+      case "remove":
+        return state.filter(student => {
+          return student.id !== action.payload.id;
+        });
+
+      // eslint-disable-next-line no-fallthrough
+      default:
+        throw new Error("Invalid!!");
+    };
+  };
+
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  //Add a student
+  //remove a student
+
+  const addStudent = (e) => {
+    e.preventDefault();
+
+    const student = {
+      id: Date.now(),
+      name,
+      email
+    };
+
+    setName('');
+    setEmail('');
+    dispatch({
+      type: "add",
+      payload: student
+    });
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Reducers</h1>
+
+        <form onSubmit={addStudent}>          
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <button type="submit">Add Student</button>         
+        </form>
+
+        <ul>
+          {state.map((student) => {
+            return (<li key={student.id}>
+              <div>
+                {student.name} --- {student.email}
+                <button onClick={() => dispatch({type: "remove", payload: {id: student.id} } )}>Remove</button>
+              </div>
+            </li>
+          );})}
+        </ul>
+
+      </div>
     </div>
   );
 }
